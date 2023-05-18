a12a9c09e82e8dafcb4b10092cedb01ed8ba3e31
Marcos Garrido Hortelano <mgarrido@nethits.com>
2023-05-18 10:31:37 +0200

    Feat: Creacion del modal de redes sociales + enlaces a las redes

--- js/index.js
+++ js/index.js
@@ -16,3 +16,23 @@ menuNavbar.onclick = function(){
 document.getElementById("habilidades").addEventListener("click", function() {
     location.reload();
 });
+
+
+var modal = document.getElementById("myModal");
+var closeButton = document.getElementsByClassName("close")[0];
+
+// Mostrar el modal al hacer clic en un botón
+document.getElementById("openModalButton").addEventListener("click", function() {
+    modal.style.display = "block";
+});
+
+// Ocultar el modal al hacer clic en el botón de cierre o fuera del modal
+closeButton.addEventListener("click", function() {
+    modal.style.display = "none";
+});
+
+window.addEventListener("click", function(event) {
+    if (event.target == modal) {
+    modal.style.display = "none";
+    }
+});
